3825014a09b016267def8145b38f43c347fbe67d
Brian Geppert <briangeppert@briangeppert.com>
2014-09-11 11:36:17 -0500

    test_glob_filter.dart: started using 'group()' and 'test()'.

--- test/test_glob_filter.dart
+++ test/test_glob_filter.dart
@@ -7,48 +7,52 @@ import "package:path/path.dart" as pathos;
 import "package:unittest/unittest.dart";
 
 void defineTests() {
-  testExclude();
-  testInclude();
+  group("GlobFilter", () {
+    testExclude();
+    testInclude();
+  });
 }
 
 void testExclude() {
   var subject = "GlobFilter.include()";
 
-  //
-  var appRoot = Directory.current.parent.path;
-  appRoot = appRoot.replaceAll("\\", "/");
-  var pattern = appRoot + "/lib/**/*.dart";
-  var lister = _getLister(pattern);
-  var list = lister.list(Directory.current.parent.path);
-  var isDirectory = (String path) => new Directory(path).existsSync();
-  var criteria = appRoot + "/**/src/glob_*.dart";
-  var filter = new GlobFilter(criteria, isDirectory: isDirectory, isWindows:
-      Platform.isWindows);
-  var result = filter.exclude(list);
-  result.sort((a, b) => a.compareTo(b));
-  result = result.map((e) => pathos.basename(e)).toList();
-  var expected = ["globbing.dart"];
-  expect(result, expected, reason: "$subject, ");
+  test(subject, () {
+    var appRoot = Directory.current.parent.path;
+    appRoot = appRoot.replaceAll("\\", "/");
+    var pattern = appRoot + "/lib/**/*.dart";
+    var lister = _getLister(pattern);
+    var list = lister.list(Directory.current.parent.path);
+    var isDirectory = (String path) => new Directory(path).existsSync();
+    var criteria = appRoot + "/**/src/glob_*.dart";
+    var filter = new GlobFilter(criteria, isDirectory: isDirectory, isWindows:
+        Platform.isWindows);
+    var result = filter.exclude(list);
+    result.sort((a, b) => a.compareTo(b));
+    result = result.map((e) => pathos.basename(e)).toList();
+    var expected = ["globbing.dart"];
+    expect(result, expected, reason: "$subject, ");
+  });
 }
 
 void testInclude() {
   var subject = "GlobFilter.include()";
 
-  //
-  var appRoot = Directory.current.parent.path;
-  appRoot = appRoot.replaceAll("\\", "/");
-  var pattern = appRoot + "/lib/**/*.dart";
-  var lister = _getLister(pattern);
-  var list = lister.list(Directory.current.parent.path);
-  var isDirectory = (String path) => new Directory(path).existsSync();
-  var criteria = appRoot + "/**/src/glob_*.dart";
-  var filter = new GlobFilter(criteria, isDirectory: isDirectory, isWindows:
-      Platform.isWindows);
-  var result = filter.include(list);
-  result.sort((a, b) => a.compareTo(b));
-  result = result.map((e) => pathos.basename(e)).toList();
-  var expected = ["glob_filter.dart", "glob_lister.dart", "glob_parser.dart"];
-  expect(result, expected, reason: "$subject, ");
+  test(subject, () {
+    var appRoot = Directory.current.parent.path;
+    appRoot = appRoot.replaceAll("\\", "/");
+    var pattern = appRoot + "/lib/**/*.dart";
+    var lister = _getLister(pattern);
+    var list = lister.list(Directory.current.parent.path);
+    var isDirectory = (String path) => new Directory(path).existsSync();
+    var criteria = appRoot + "/**/src/glob_*.dart";
+    var filter = new GlobFilter(criteria, isDirectory: isDirectory, isWindows:
+        Platform.isWindows);
+    var result = filter.include(list);
+    result.sort((a, b) => a.compareTo(b));
+    result = result.map((e) => pathos.basename(e)).toList();
+    var expected = ["glob_filter.dart", "glob_lister.dart", "glob_parser.dart"];
+    expect(result, expected, reason: "$subject, ");
+  });
 }
 
 GlobLister _getLister(String pattern) {
